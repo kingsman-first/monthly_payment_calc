8f21cf45124eb0a9191304bf81d0deda184b3fb4
Maryan Gelyk <maryangelyk16@gmail.com>
2021-10-13 14:36:26 -0400

    Added files

---
+++ Monthly_payment_cacl/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace Monthly_payment_cacl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPurchPrize = new System.Windows.Forms.Label();
+            this.labelDPAmmount = new System.Windows.Forms.Label();
+            this.labelInterestRate = new System.Windows.Forms.Label();
+            this.labelLoanTerm = new System.Windows.Forms.Label();
+            this.labelAmmFinance = new System.Windows.Forms.Label();
+            this.labelMonthlyPaym = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelPurchPrize
+            // 
+            this.labelPurchPrize.AutoSize = true;
+            this.labelPurchPrize.Location = new System.Drawing.Point(82, 48);
+            this.labelPurchPrize.Name = "labelPurchPrize";
+            this.labelPurchPrize.Size = new System.Drawing.Size(82, 13);
+            this.labelPurchPrize.TabIndex = 0;
+            this.labelPurchPrize.Text = "Purchase Price:";
+            this.labelPurchPrize.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelDPAmmount
+            // 
+            this.labelDPAmmount.AutoSize = true;
+            this.labelDPAmmount.Location = new System.Drawing.Point(35, 90);
+            this.labelDPAmmount.Name = "labelDPAmmount";
+            this.labelDPAmmount.Size = new System.Drawing.Size(129, 13);
+            this.labelDPAmmount.TabIndex = 1;
+            this.labelDPAmmount.Text = "Down Payment Ammount:";
+            this.labelDPAmmount.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelInterestRate
+            // 
+            this.labelInterestRate.AutoSize = true;
+            this.labelInterestRate.Location = new System.Drawing.Point(51, 138);
+            this.labelInterestRate.Name = "labelInterestRate";
+            this.labelInterestRate.Size = new System.Drawing.Size(113, 13);
+            this.labelInterestRate.TabIndex = 2;
+            this.labelInterestRate.Text = "Interest Rate (Annual):";
+            this.labelInterestRate.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelLoanTerm
+            // 
+            this.labelLoanTerm.AutoSize = true;
+            this.labelLoanTerm.Location = new System.Drawing.Point(59, 186);
+            this.labelLoanTerm.Name = "labelLoanTerm";
+            this.labelLoanTerm.Size = new System.Drawing.Size(105, 13);
+            this.labelLoanTerm.TabIndex = 3;
+            this.labelLoanTerm.Text = "Loan Term (Months):";
+            this.labelLoanTerm.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelAmmFinance
+            // 
+            this.labelAmmFinance.AutoSize = true;
+            this.labelAmmFinance.Location = new System.Drawing.Point(457, 48);
+            this.labelAmmFinance.Name = "labelAmmFinance";
+            this.labelAmmFinance.Size = new System.Drawing.Size(107, 13);
+            this.labelAmmFinance.TabIndex = 4;
+            this.labelAmmFinance.Text = "Ammount to Finance:";
+            this.labelAmmFinance.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelMonthlyPaym
+            // 
+            this.labelMonthlyPaym.AutoSize = true;
+            this.labelMonthlyPaym.Location = new System.Drawing.Point(473, 90);
+            this.labelMonthlyPaym.Name = "labelMonthlyPaym";
+            this.labelMonthlyPaym.Size = new System.Drawing.Size(91, 13);
+            this.labelMonthlyPaym.TabIndex = 5;
+            this.labelMonthlyPaym.Text = "Monthly Payment:";
+            this.labelMonthlyPaym.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(184, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(184, 131);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(184, 183);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(590, 45);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(590, 87);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(52, 390);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(686, 61);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(605, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelMonthlyPaym);
+            this.Controls.Add(this.labelAmmFinance);
+            this.Controls.Add(this.labelLoanTerm);
+            this.Controls.Add(this.labelInterestRate);
+            this.Controls.Add(this.labelDPAmmount);
+            this.Controls.Add(this.labelPurchPrize);
+            this.Name = "Form1";
+            this.Text = "Monthly Payment Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPurchPrize;
+        private System.Windows.Forms.Label labelDPAmmount;
+        private System.Windows.Forms.Label labelInterestRate;
+        private System.Windows.Forms.Label labelLoanTerm;
+        private System.Windows.Forms.Label labelAmmFinance;
+        private System.Windows.Forms.Label labelMonthlyPaym;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
